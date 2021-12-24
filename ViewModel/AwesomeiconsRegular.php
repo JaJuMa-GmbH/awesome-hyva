240810c7a6689ab929ab94181a0539c1847c3ff0
Oli <jaufmann@jajuma.de>
2021-12-24 08:24:11 +0700

    Update Licenses

--- ViewModel/AwesomeiconsRegular.php
+++ ViewModel/AwesomeiconsRegular.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * Hyvä Themes - https://hyva.io
- * Copyright © Hyvä Themes 2020-present. All rights reserved.
- * This product is licensed per Magento install
- * See https://hyva.io/license
+ * @author    JaJuMa GmbH <info@jajuma.de>
+ * @copyright Copyright (c) 2022-present JaJuMa GmbH <https://www.jajuma.de>. All rights reserved.
+ * @license   http://opensource.org/licenses/mit-license.php MIT License
  */
 
 declare(strict_types=1);
